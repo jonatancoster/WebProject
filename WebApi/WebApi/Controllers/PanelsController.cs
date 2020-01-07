31e73a30ad9a380f5a72b5d3b3860f6325e5e211
jonatancoster <jonatan.coster@gmail.com>
2020-01-07 18:06:02 +0100

    Add project files.

---
+++ WebApi/WebApi/Controllers/PanelsController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using WebApi.Models;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "http://localhost:58088", headers: "*", methods: "*", SupportsCredentials = true)]
+    [RoutePrefix("api/panels")]
+    public class PanelsController : ApiController
+    {
+
+        /// <summary>
+        /// Returns an array containing the names of all stored panels.
+        /// GET: api/panels
+        /// </summary>
+        /// <returns>An array containing the names of all stored panels.</returns>
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult GetPanels()
+        {
+            List<String> panels = new List<String>();
+            foreach(string key in HttpContext.Current.Session.Keys)
+            {
+                System.Diagnostics.Debug.WriteLine("Panel: " + key);
+                panels.Add(key);
+            }
+            return Json(panels.ToArray());
+        }
+
+        /// <summary>
+        /// Returns the panel with the specified name, or HTTP response "404 Not Found" if no such panel exists.
+        /// GET: api/panels/name
+        /// </summary>
+        /// <param name="name">The name of the panel to return.</param>
+        /// <returns>The panel with the specified name, or HTTP response "404 Not Found" if no such panel exists.</returns>
+        [Route("{name}")]
+        [HttpGet]
+        public IHttpActionResult GetPanel(string name)
+        {
+            // Return the panel with the corresponding name if it exist.
+            if (HttpContext.Current.Session[name] != null)
+            {
+                System.Diagnostics.Debug.WriteLine("Returned panel: " + name);
+                return Json(HttpContext.Current.Session[name]);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("No panel named: " + name);
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Stores a panel in the current session, provided that no panel with the same name already exists.
+        /// POST: api/panels
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>
+        /// HTTP response "200 OK" if the panel is stored successfully.
+        /// HTTP response "409 Conflict" if a panel with the same name already exists.
+        /// HTTP response "400 Bad Request" if the received data does not conform to the Panel model.
+        /// </returns>
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]Panel value)
+        {
+            if(ModelState.IsValid)
+            {
+                if (HttpContext.Current.Session[value.Name] == null)
+                {
+                    HttpContext.Current.Session[value.Name] = value;
+                    System.Diagnostics.Debug.WriteLine("Created: " + value.Name);
+                    return Ok(value.Name);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Panel already exists: " + value.Name);
+                    return Conflict();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Replaces an already existing panel.
+        /// PUT: api/panels/name
+        /// </summary>
+        /// <param name="name">The name of the panel.</param>
+        /// <param name="value">The panel.</param>
+        /// <returns>
+        /// HTTP response "200 OK" if the panel is successfully replaced.
+        /// HTTP response "404 Not Found" if no panel with the specified name exists.
+        /// </returns>
+        [Route("{name}")]
+        [HttpPut]
+        public IHttpActionResult Put(string name, [FromBody]Panel value)
+        {
+            if(HttpContext.Current.Session[name] != null)
+            {
+                HttpContext.Current.Session[value.Name] = value;
+                System.Diagnostics.Debug.WriteLine("Replaced: " + value.Name);
+                return Ok(value.Name);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Deletes a panel from the current session.
+        /// DELETE: api/panels/name
+        /// </summary>
+        /// <param name="name">The name of the panel to delete.</param>
+        /// <returns>
+        /// HTTP response "200 OK" if the panel is successfully deleted.
+        /// HTTP response "404 Not Found" if no panel with the specified name exists.
+        /// </returns>
+        [Route("{name}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(string name)
+        {
+            if(HttpContext.Current.Session[name] == null)
+            {
+                return NotFound();
+            } else
+            {
+                HttpContext.Current.Session.Remove(name);
+                System.Diagnostics.Debug.WriteLine("Deleted: " + name);
+                return Ok();
+            }
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication/WebApplication/app/app.js
@@ -0,0 +1,134 @@
+﻿var app = angular.module('myApp', []);
+
+var apiUrl = 'http://localhost:55748/Api/Panels/';
+
+
+
+app.controller('myCtrl', function ($scope, $http) {
+
+    // The html class names for the squares. The state of a square is used as an index to get the corresponding class name.
+    $scope.htmlClasses = ["state0", "state1", "state2"];
+
+    // This is the panel that is rendered on screen. By default each square has state 0.
+    $scope.panel = {
+        name: "panel",
+        squares: []
+    };
+    for (var i = 0; i < 36; i++) {
+        $scope.panel.squares.push({ state: 0 });
+    }
+
+    /**
+     * @description Retrieves the names of the panels stored on the server.
+     */
+    $scope.getPanels = function () {
+        $http({
+            method: 'GET',
+            url: apiUrl,
+            withCredentials: true
+        }).then(function successCallback(response) {
+            console.log("Retrieved panels:" + '\n' + response.data);
+            $scope.panelNames = response.data;
+        }, function errorCallback(response) {
+                console.error("An error occurred while retreiving panels");
+        });
+    }
+
+    /**
+     * @description Retrieves the panel with the specified name from the server.
+     * @param {string} panelName The name of the panel to retrieve.
+     */
+    $scope.getPanel = function (panelName) {
+        var panelUrl = encodeURI(apiUrl + panelName);
+        $http({
+            method: 'GET',
+            url: panelUrl,
+            withCredentials: true
+        }).then(function successCallback(response) {
+            console.log("Retrieved panel from: " + panelUrl);
+            $scope.panel.name = response.data.Name;
+            for (var i = 0; i < response.data.Squares.length; i++) {
+                $scope.panel.squares[i].state = response.data.Squares[i].State;
+            }
+        }, function errorCallback(response) {
+                console.error("An error occurred while retreiving panel from " + panelUrl);
+        });
+    }
+
+    /**
+     * @description Stores the currently displayed panel on the server. 
+     *              If the panel already exists, the user is asked if it should be replaced.
+     */
+    $scope.savePanel = function () {
+        $http({
+            method: 'POST',
+            url: apiUrl,
+            withCredentials: true,
+            data: $scope.panel
+        }).then(function successCallback(response) {
+            console.log("Saved panel: " + $scope.panel.name);
+            $scope.getPanels();
+        }, function errorCallback(response) {
+            console.error("An error occurred while saving panel " + $scope.panel.name);
+            if (response.status == 409) {
+                if (confirm("A configuration with this name already exist. Do you wish to overwrite it?")) {
+                    $scope.replacePanel();
+                }
+            }
+        });
+    }
+
+    /**
+     * @description Deletes a panel with the specified name from the server.
+     * @param {any} panelName The name of the panel to delete.
+     */
+    $scope.deletePanel = function (panelName) {
+        var panelUrl = encodeURI(apiUrl + panelName);
+        $http({
+            method: 'DELETE',
+            url: panelUrl,
+            withCredentials: true
+        }).then(function successCallback(response) {
+            console.log("Deleted panel at: " + panelUrl);
+            $scope.getPanels();
+        }, function errorCallback(response) {
+            console.error("An error occurred while deleting panel at " + panelUrl);
+        });
+    }
+
+    /**
+     * @description Replaces a panel which is already stored on the server.
+     */
+    $scope.replacePanel = function () {
+        var panelUrl = encodeURI(apiUrl + $scope.panel.name);
+        $http({
+            method: 'PUT',
+            url: panelUrl,
+            withCredentials: true,
+            data: $scope.panel
+        }).then(function successCallback(response) {
+            console.log("Replaced panel: " + $scope.panel.name);
+            $scope.getPanels();
+        }, function errorCallback(response) {
+            console.error("An error occurred while replacing panel " + $scope.panel.name);
+        });
+    }
+
+
+
+    $scope.getPanels();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
